cea731cb5eb8607fcefbf666252ea7fef9ac30ad
fabricio vaz <fabricio.vaz2004@gmail.com>
2023-04-04 08:24:54 -0300

    Criada uma Function para gerar uma letra nova, e alterado o local de atualização da letra e limpeza do input

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-
-const indice = Math.floor(Math.random() * letras.length);
-let letraAleatoria = letras[indice];
 let wd1 = document.getElementById(`wd1`);
-wd1.innerHTML = letraAleatoria;
-
-
 let pontuacao = 0;
+
+function novaLetra() {
+  const indice = Math.floor(Math.random() * letras.length);
+  let letraAleatoria = letras[indice];
+  wd1.innerHTML = letraAleatoria;
+}
+
+novaLetra();
+
+let inputAtual = document.getElementById('input_box');
+
 inputAtual.addEventListener('keydown', function(event) {
   if (event.code === 'Enter') {
-    let atual = document.getElementById(`wd1`).textContent;
-    let inputAtual = document.getElementById('input_box').value;
-    let pontos = documento.getElementById('pontos');
-    if (atual === inputAtual) {
-        pontuacao++;
-        pontos.innerHTML = pontuacao;
-      }
-    }
+    let atual = wd1.textContent;
+    let valorInputAtual = inputAtual.value;
+    let pontos = document.getElementById('pontos');
+    if (atual === valorInputAtual) {
+      pontuacao++;
+      pontos.innerHTML = pontuacao;
+      novaLetra();
+      inputAtual.value = '';
+    }
+  }
 });
