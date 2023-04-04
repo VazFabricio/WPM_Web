ff7650591071c8de97715a2c98cdae83fec0126d
fabricio vaz <fabricio.vaz2004@gmail.com>
2023-04-04 09:10:03 -0300

    Gerar mais de uma letra

--- index.js
+++ index.js
@@ -3,15 +3,40 @@ const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 let wd1 = document.getElementById(`wd1`);
 let pontuacao = 0;
 
+let arr = new Array(5);
+
 function novaLetra() {
   const indice = Math.floor(Math.random() * letras.length);
   let letraAleatoria = letras[indice];
-  wd1.innerHTML = letraAleatoria;
+  return letraAleatoria;
 }
 
-novaLetra();
+
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = novaLetra();
+}
+
+for (let i = 1; i <= arr.length; i++) {
+  let wd = document.getElementById(`wd${i}`);
+  wd.innerHTML = arr[i - 1];
+}
+
 
 let inputAtual = document.getElementById('input_box');
+
+inputAtual.addEventListener('input', function() {
+  let inputValor = inputAtual.value;
+  let atual = wd1.textContent;
+
+  if (inputValor === atual) {
+    wd1.classList.add('correto');
+    wd1.classList.remove('incorreto');
+  } else {
+    wd1.classList.add('incorreto');
+    wd1.classList.remove('correto');
+  }
+});
+
 
 inputAtual.addEventListener('keydown', function(event) {
   if (event.code === 'Enter') {
@@ -23,6 +48,7 @@ inputAtual.addEventListener('keydown', function(event) {
       pontos.innerHTML = pontuacao;
       novaLetra();
       inputAtual.value = '';
+      wd1.classList.remove('correto')
     }
   }
 });
